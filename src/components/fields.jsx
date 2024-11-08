f42ec45b12d73b807ba5f1a7aea06ce48022977a
baldx <mr.ahmad.m2006@gmail.com>
2024-11-08 10:56:37 +0100

    create functions & fields

---
+++ src/components/fields.jsx
@@ -0,0 +1,28 @@
+function Field({label, inputType}) {
+
+    function capitalizeLetter(string) { //function to capitalize letters 
+        return string.charAt(0).toUpperCase() + string.slice(1); //* takes first character of string and sets to uppercase, then pastes the rest of the string 
+    }
+
+    function removeSpace(string) { //removes spaces between words. used for name & id attribute
+        const newString = camelCasing(string);
+        return newString.split(' ').join('');
+    }
+
+    function camelCasing(string) { //camelCasing for attributes
+        return string.split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join('');
+    }
+
+    return (
+        <>
+            <label htmlFor={label}>
+                {capitalizeLetter(label) + ":"}
+                <input type={inputType} name={removeSpace(label)} id={removeSpace(label)} />
+            </label>
+        </>
+    )
+}
+
+export default Field;
