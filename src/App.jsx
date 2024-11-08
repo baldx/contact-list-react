5356519ed427b877c6c7c1748cfd9cee447d2dce
baldx <mr.ahmad.m2006@gmail.com>
2024-11-08 13:18:31 +0100

    pass through contact data to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 import Form from './components/form'
+import Contacts from './components/Contacts';
 
 function App() {
+  const [contacts, setContacts] = useState([
+    {firstName: "John", lastName: "Doe", birthDate: "2001-04-16"},
+    {firstName: "Boe", lastName: "Jiden", birthDate: "1899-03-21"}
+  ]);
+
+  function addContact(newContact) { //appends new contacts to contacts array
+    setContacts(prevContact => [...prevContact, newContact]); //use spread operator to add previous contacts + new ones
+  }
 
   return (
     <>
-      <Form/>
+      <Form addContact={addContact}/>
+      <Contacts contacts={contacts}/>
     </>
   )
 }
