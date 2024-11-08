195bb20cc9c738c12305f839d8b3bfd725119d7b
baldx <mr.ahmad.m2006@gmail.com>
2024-11-08 13:20:07 +0100

    add logic for saving form values & using it

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,14 +1,28 @@
-import Field from "./fields";
+import Field from "./Fields";
 
-function Form({}) {
+function Form({addContact}) {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        const firstName = e.target.elements.FirstName.value; //get the values
+        const lastName = e.target.elements.LastName.value;
+        const birthDate = e.target.elements.BirthDate.value;
+
+        const newContact = { firstName, lastName, birthDate }; //add them to a new contact
+
+        addContact(newContact); //update contact state
+
+        e.target.reset();
+    }
 
     return (
         <>
-            <form>
-            <Field label="First name" inputType="text" />
-            <Field label="Last name" inputType="text" />
-            <Field label="Birth date" inputType="date" />
-            <button type="submit" onClick={e => e.preventDefault()}>Submit</button>
+            <form onSubmit={handleSubmit}>
+            <Field label="First name" inputType="text" value=""/>
+            <Field label="Last name" inputType="text" value=""/>
+            <Field label="Birth date" inputType="date" value=""/>
+            <button type="submit">Submit</button>
             </form>
         </>
     )
