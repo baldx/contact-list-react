e2923a2f57923c095695ac121539980527781aa4
baldx <mr.ahmad.m2006@gmail.com>
2024-11-15 09:10:21 +0100

    add calculateAge func

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,14 +1,29 @@
 function Contacts({contacts}) {
-console.log(contacts);
+
+    const calculateAge = (birthdate) => {
+        const today = new Date(); //get todays date
+        const birthDate = new Date(birthdate); //get bithdays date
+
+        let age = today.getFullYear() - birthDate.getFullYear(); //calulcate age diff
+        const monthDiff = today.getMonth() - birthDate.getMonth(); //calculate month diff
+
+        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) { //logic to check when birthday is in same month
+            age--;
+        }
+
+        return age;
+    }
+    
 
     return (
         <>
             <ul>
                 {contacts.map((contact, index) => {
                     return <li>
-                        <div className="firstName">{contact.firstName}</div>
-                        <div className="lastName">{contact.lastName}</div>
-                        <div className="birthDate">{contact.birthDate}</div>
+                        <div className="firstName" key={contact.firstName + index}>Name: {contact.firstName}</div>
+                        <div className="lastName" key={contact.lastName + index}>Last name: {contact.lastName}</div> {/* display different contact info */}
+                        <div className="birthDate" key={contact.birthDate + index}>Birthday: {contact.birthDate}</div>
+                        <div className="age" key={"daysLeft" + index}>{"Age: " + calculateAge(contact.birthDate)}</div>
                     </li>
                 })}
             </ul>
