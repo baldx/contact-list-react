31af5f651f031b639f58b5dbd4988c5e51aa7268
baldx <mr.ahmad.m2006@gmail.com>
2024-11-15 09:22:29 +0100

    add comment

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Form from './components/form'
 import Contacts from './components/Contacts';
 
 function App() {
-  const [contacts, setContacts] = useState([
+  const [contacts, setContacts] = useState([ //default state
     {firstName: "John", lastName: "Doe", birthDate: "2001-04-16"},
     {firstName: "Boe", lastName: "Jiden", birthDate: "1899-03-21"}
   ]);
 
   function addContact(newContact) { //appends new contacts to contacts array
     setContacts(prevContact => [...prevContact, newContact]); //use spread operator to add previous contacts + new ones
-  }
+  };
+
+  
 
   return (
     <>
