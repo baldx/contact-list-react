088abb358b8efb403240e48fe18afd62644490d0
baldx <125921176+baldx@users.noreply.github.com>
2024-11-17 00:57:16 +0100

    add formValidation

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -12,16 +12,40 @@ function Form({addContact, calculateAge, calculateDaysTillBirthday}) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        
+
         const firstName = e.target.elements.FirstName.value; //get the values
         const lastName = e.target.elements.LastName.value;
         const birthDate = e.target.elements.BirthDate.value;
 
+        if (!formValidation(firstName, lastName, birthDate)) {
+            alert('Fill in the form, with no numbers')
+            return; //if formValidation is not true, return, which exits the function
+        }
+        
         const newContact = { firstName, lastName, birthDate, creation: increaseCreationID(), age: calculateAge(birthDate), daysLeft: calculateDaysTillBirthday(birthDate)}; //add them to a new contact
 
         addContact(newContact); //update contact state
 
         e.target.reset(); //resets the form
+        return false;
+    }
+
+    function formValidation(firstName, lastName, birthDate) {
+
+        function checkNumbers(string) {
+            let containsNumbers = false;
+
+            string.split(' ').forEach(element => { //takes string into array then compares each element (each letter) if its a number. 
+                if (typeof element === 'number') containsNumbers = true;
+            });
+            return containsNumbers;
+        }
+
+        const isFirstNameValid = firstName.trim() !== '' && !checkNumbers(firstName); //.trim() - removes whitespace in string
+        const isLastNameValid = lastName.trim() !== '' && !checkNumbers(lastName);
+        const isBirthDateValid = !isNaN(Date.parse(birthDate)); // check if the date is valid
+
+        return isFirstNameValid && isLastNameValid && isBirthDateValid;
     }
 
     return (
